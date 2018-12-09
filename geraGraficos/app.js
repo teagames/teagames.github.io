13ebc239537fda9171ff470df8f3fe3797a42a44
teagames <spi2018.tcc@gmail.com>
2018-12-09 10:45:51 -0200

    GeraGraficos adicionada

---
+++ geraGraficos/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA23clfCWrLSD0BVFoXhyj1opi8ZcqaTtk",
+    authDomain: "teagames-3193c.firebaseapp.com",
+    databaseURL: "https://teagames-3193c.firebaseio.com",
+    projectId: "teagames-3193c",
+    storageBucket: "teagames-3193c.appspot.com",
+    messagingSenderId: "848455715190"
+  };
+  firebase.initializeApp(config);
